54388d89612557f5593e7609becafa16106f6cbc
janoglezcampos <janoglezcampos@gmail.com>
2021-06-28 18:47:42 +0200

    Cambios de como se asignan algunas propiedades a los sockets para hacer funcionar server stapling

--- segB/src/server/Server.java
+++ segB/src/server/Server.java
@@ -21,7 +21,7 @@ public class Server {
 	private static KeyStore trust;
 	private static KeyStore key;
 	private static int contador=0;
-	private static boolean ocspEnable = false;
+	private static boolean ocspEnable = true;
 	private static String serverAuthCert = "serverauth";
 	
 	private static PublicKey clientPublicKey;
@@ -60,33 +60,136 @@ public class Server {
 			}
 			System.out.println("INICIANDO CONEXION");
 			start(args[0],args[1],args[2],args[3]);
-		} catch (UnrecoverableKeyException | KeyManagementException | NoSuchAlgorithmException | CertificateException
-				| KeyStoreException | IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 
 	}
+	
+    static class ServerParameters {
+        boolean enabled = true;
+        int cacheSize = 256;
+        int cacheLifetime = 3600;
+        int respTimeout = 5000;
+        String respUri = "http://localhost:9999";
+        boolean respOverride = true;
+        boolean ignoreExts = false;
+        String[] protocols = new String[]{ "TLSv1.2" };
+        String[] ciphers = null;
 
-	public static void start(String keyStorePath, String trustStorePath, String password, String chipherAlgoritm) throws IOException, UnrecoverableKeyException, NoSuchAlgorithmException, CertificateException, KeyStoreException, KeyManagementException {
+        ServerParameters() { }
+    }
+
+    static class CustomizedServerSocketFactory extends SSLServerSocketFactory {
+        final SSLContext sslc;
+        final String[] protocols;
+        final String[] cipherSuites;
+
+        CustomizedServerSocketFactory(SSLContext ctx, String[] prots, String[] suites)
+                throws GeneralSecurityException {
+            super();
+            sslc = (ctx != null) ? ctx : SSLContext.getDefault();
+            protocols = prots;
+            cipherSuites = suites;
+
+            // Create the Trust Manager Factory using the PKIX variant
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("PKIX");
+        }
+
+        @Override
+        public ServerSocket createServerSocket(int port) throws IOException {
+            ServerSocket sock =
+                    sslc.getServerSocketFactory().createServerSocket(port);
+            customizeSocket(sock);
+            return sock;
+        }
+
+        @Override
+        public ServerSocket createServerSocket(int port, int backlog)
+                throws IOException {
+            ServerSocket sock =
+                    sslc.getServerSocketFactory().createServerSocket(port,
+                            backlog);
+            customizeSocket(sock);
+            return sock;
+        }
+
+        @Override
+        public ServerSocket createServerSocket(int port, int backlog,
+                InetAddress ifAddress) throws IOException {
+            ServerSocket sock =
+                    sslc.getServerSocketFactory().createServerSocket(port,
+                            backlog, ifAddress);
+            customizeSocket(sock);
+            return sock;
+        }
+
+        @Override
+        public String[] getDefaultCipherSuites() {
+            return sslc.getDefaultSSLParameters().getCipherSuites();
+        }
+
+        @Override
+        public String[] getSupportedCipherSuites() {
+            return sslc.getSupportedSSLParameters().getCipherSuites();
+        }
+
+        private void customizeSocket(ServerSocket sock) {
+            if (sock instanceof SSLServerSocket) {
+                if (protocols != null) {
+                    ((SSLServerSocket)sock).setEnabledProtocols(protocols);
+                }
+                if (cipherSuites != null) {
+                    ((SSLServerSocket)sock).setEnabledCipherSuites(cipherSuites);
+                }
+            }
+        }
+    }
+        
+
+	public static void start(String keyStorePath, String trustStorePath, String password, String chipherAlgoritm) throws Exception {
 		System.out.println("INICIANDO ALMACENES");
+		ServerParameters servParams = new ServerParameters();
+		
+        System.setProperty("jdk.tls.server.enableStatusRequestExtension",
+                Boolean.toString(servParams.enabled));
+
+        // Set all the other operating parameters
+        System.setProperty("jdk.tls.stapling.cacheSize",
+                Integer.toString(servParams.cacheSize));
+        System.setProperty("jdk.tls.stapling.cacheLifetime",
+                Integer.toString(servParams.cacheLifetime));
+        System.setProperty("jdk.tls.stapling.responseTimeout",
+                Integer.toString(servParams.respTimeout));
+        System.setProperty("jdk.tls.stapling.responderURI", servParams.respUri);
+        System.setProperty("jdk.tls.stapling.responderOverride",
+                Boolean.toString(servParams.respOverride));
+        System.setProperty("jdk.tls.stapling.ignoreExtensions",
+                Boolean.toString(servParams.ignoreExts));
 		store(keyStorePath,trustStorePath,password);
-		int port=8080;
+		int port=443;
 
 
 		SSLContext sc = SSLContext.getInstance("TLS");
 		final X509KeyManager origKm = (X509KeyManager)keyManagers[0];
 		X509KeyManager km = new CustomKeyManager(serverAuthCert, origKm);
 		sc.init(new KeyManager[] { km }, trustManagers, null);
+		
+		SSLServerSocketFactory sslssf = new CustomizedServerSocketFactory(sc,
+                servParams.protocols, servParams.ciphers);
 
-		SSLServerSocketFactory ssf = sc.getServerSocketFactory();
-		ServerSocket serverSocket1 = ssf.createServerSocket(port);
+		//SSLServerSocketFactory ssf = sc.getServerSocketFactory();
+		//ServerSocket serverSocket1 = ssf.createServerSocket(port);
+		ServerSocket serverSocket1 = (SSLServerSocket) sslssf.createServerSocket(port);
 		((SSLServerSocket)serverSocket1).setNeedClientAuth(true);
+		//((SSLServerSocket) serverSocket1).setEnabledProtocols(protocols);
 		System.out.println("Esperando conexión... (OCSP habilitado: "+ System.getProperty("jdk.tls.server.enableStatusRequestExtension") +")");
 
 		while (true) {			
 			Socket aClient = serverSocket1.accept();
+			System.out.println(aClient.getRemoteSocketAddress().toString());
 			System.out.println("Client accepted");
 			aClient.setSoLinger(true, 10000);
 
