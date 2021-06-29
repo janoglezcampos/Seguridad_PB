c02291fb9949fdb37993e1b7bb7a6305a39391ea
janoglezcampos <janoglezcampos@gmail.com>
2021-06-29 13:06:22 +0200

    Generación correcta de respuestas y validacion de certificado del cliente aka funcionalidad 3 al completo

--- segB/src/server/Util3.java
+++ segB/src/server/Util3.java
@@ -5,21 +5,14 @@ import common.*;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.*;
-import java.security.AlgorithmParameters;
-import java.security.Key;
-import java.security.PublicKey;
-import java.security.KeyStore.PasswordProtection;
-import java.security.KeyStore.SecretKeyEntry;
 import java.security.cert.*;
 import java.util.ArrayList;
-import javax.crypto.*;
-
-import clientpart.Client;
 
 
 public class Util3 {
 	private static String savePath="/Users/lexy/Desktop/Clases/Seguridad/serverSavedFiles/";
 	private static String secretKeyAlias = "dataenckey";
+	private static String signAlias = "serversign";
 
 	public static void start(Socket aClient, String algorithm, String passwd_key) {
 		new Thread() {
@@ -27,22 +20,24 @@ public class Util3 {
 				try {
 					DataInputStream input= new DataInputStream(aClient.getInputStream());
 
-					byte [] cert_rec= input.readNBytes(input.readInt());
+					byte [] certAuth= input.readNBytes(input.readInt());
 					String id_registro=new String(input.readNBytes(input.readInt()));  
 
-					InputStream in = new ByteArrayInputStream(cert_rec);
+					InputStream in = new ByteArrayInputStream(certAuth);
 					CertificateFactory cf   = CertificateFactory.getInstance("X.509");
 					Certificate certificate = cf.generateCertificate(in);
-					String idPropietario = ((X509Certificate)certificate).getIssuerDN().toString();
+					Certificate serverCertSign = Server.getKeyStore().getCertificate(signAlias);
 
-					PrintWriter output = new PrintWriter(aClient.getOutputStream());
-   
 					File directorio= new File (savePath);
 					String carpeta_comprobar = "";
+
+
+					ObjectOutputStream response = new ObjectOutputStream(aClient.getOutputStream());
+					Response res = null;
+
 					boolean esprivado=false;
 
 					if(Integer.parseInt(id_registro)<=Server.getContador()) {
-
 						File [] contenido =directorio.listFiles();
 						ArrayList <String>  contenido2 = new ArrayList <String>();
 
@@ -60,64 +55,39 @@ public class Util3 {
 							}
 						}
 
-						DataOutputStream out = new DataOutputStream(aClient.getOutputStream());
 						ArrayList<byte[]> req = new ArrayList <byte[]>();
 
+						String idPropietario = ((X509Certificate)certificate).getIssuerDN().toString();
 						if(esprivado) {
-							if ((id_registro+"_"+idPropietario).equals(carpeta_comprobar)) {
-								System.out.println("COMPROBACIÓN CORRECTA DE ID: " + carpeta_comprobar);
-								req=es_privado(carpeta_comprobar);
-								out.writeInt(req.get(1).length);
-								out.write(req.get(1));
-								out.writeInt(req.get(2).length);
-								out.write(req.get(2));
-								out.writeInt(req.get(3).length);
-								out.write(req.get(3));
-								out.writeInt(req.get(4).length);
-								out.write(req.get(4));
-								out.writeInt(req.get(0).length);
-								out.write(req.get(0));
-								
-								byte [] decriptedFile = Encription.decriptDocument(req.get(5), secretKeyAlias, passwd_key, algorithm, Server.getLocalCipherParams(), Server.getKeyStore());
-								req = Encription.encript2sendPGP(req, decriptedFile, Server.getClientPublicKey());
-								
-								out.writeInt(req.get(6).length);//Archivo encriptado
-								out.write(req.get(6));
-								out.writeInt(req.get(7).length);//Cipher parameters
-								out.write(req.get(7));
-								out.writeInt(req.get(8).length);//Encripted key
-								out.write(req.get(8));
-								out.flush();
+							if(Validation.validateCert(certificate, Server.getTrust())) {
+								if ((id_registro+"_"+idPropietario).equals(carpeta_comprobar)) {
+									System.out.println("COMPROBACIÓN CORRECTA DE ID: " + carpeta_comprobar);
 
-							} else {
-								output.println("ACCESO NO PERMITIDO\n");
-								output.close();
+									req=es_privado(carpeta_comprobar);
+
+									byte [] decriptedFile = Encription.decriptDocument(req.get(5), secretKeyAlias, passwd_key, algorithm, Server.getLocalCipherParams(), Server.getKeyStore());
+									req = Encription.encript2sendPGP(req, decriptedFile, Server.getClientPublicKey());
+									res = new Response(Integer.parseInt(id_registro), idPropietario, req.get(3).toString(), req.get(6), req.get(7), req.get(8), req.get(4), serverCertSign);
+									
+								} else {
+									res = new Response(-3);
+								}
+							}else {
+								res = new Response(-1);
 							}
 						}
-						else { 
+						else {
 							req=es_publico(carpeta_comprobar);
-							out.writeInt(req.get(1).length);
-							out.write(req.get(1));
-							out.writeInt(req.get(2).length);
-							out.write(req.get(2));
-							out.writeInt(req.get(3).length);
-							out.write(req.get(3));
-							out.writeInt(req.get(4).length);
-							out.write(req.get(4));
-							out.writeInt(req.get(0).length);
-							out.write(req.get(0));
-							out.writeInt(req.get(5).length);
-							out.write(req.get(5));
-							out.flush();
+							res = new Response(Integer.parseInt(id_registro), idPropietario, req.get(3).toString(), req.get(5),req.get(4), serverCertSign);
 						}
-
 					}else {
-						output.println("DOCUMENTO NO EXISTENTE\n");
-						output.close();
+						res = new Response(-4);
 					}
-					output.println("ok\n");
-					output.close();
-
+					response.writeObject(res);
+					response.flush();
+					response.close();
+					in.close();
+					aClient.close();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -130,7 +100,7 @@ public class Util3 {
 	public static ArrayList<byte[]> es_publico(String carpeta_comprobar) throws IOException{
 		ArrayList<byte[]> req = new ArrayList <byte[]>();
 		String ruta= savePath+carpeta_comprobar;
-		
+
 		File directorio2= new File (ruta);
 		String [] names2= {"confidencialidad","firmaDocumento","idRegistro","selloTemporal","firmaSigRD"};
 		File [] contenido =directorio2.listFiles();
@@ -156,7 +126,6 @@ public class Util3 {
 		}
 
 		for (int i=0; i< contenido2.size(); i ++) {
-
 			if (!names.contains(contenido2.get(i).getName())) {
 				byte [] file2 =Files.readAllBytes(contenido2.get(i).toPath());
 				req.add(file2);
@@ -175,23 +144,23 @@ public class Util3 {
 		File [] contenido =directorio2.listFiles();
 
 		System.out.println("Archivos: "+ contenido.toString());
-		
+
 		for(int i=0; i<names2.length; i++) {
 			byte [] file =Files.readAllBytes(Paths.get(ruta,names2[i]));
 			req.add(file);
 		}
-		
+
 		ArrayList <String>  names = new ArrayList <String>();	
 		for (int k=0; k<names2.length;k++) {
 			names.add(names2[k]);
 		} 
-		
+
 		ArrayList <File>  contenido2 = new ArrayList <File>();	
 
 		for(int i=0; i<contenido.length; i++) {
 			contenido2.add(contenido[i]);
 		}
-		
+
 		for (int i=0; i< contenido2.size(); i ++) {
 			if (!names.contains(contenido2.get(i).getName())) {
 				byte [] file2 =Files.readAllBytes(contenido2.get(i).toPath());
