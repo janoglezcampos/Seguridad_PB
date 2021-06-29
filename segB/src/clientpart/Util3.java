ec147afb4424e4731b66393de012aaed38d23626
janoglezcampos <janoglezcampos@gmail.com>
2021-06-29 22:42:05 +0200

    Se utilizan las entradas de databaseentry para generar las respuestas al cliente, ya no se depende de listas en memoria si no que se comprueba la base de datos en memoria, y fixes a como se recuperan los archivos publicos en el cliente.

--- segB/src/clientpart/Util3.java
+++ segB/src/clientpart/Util3.java
@@ -2,38 +2,18 @@ package clientpart;
 
 import common.*;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
-import java.nio.file.Files;
 import java.security.*;
 import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-
-import server.Server;
-import server.Util;
-
 public class Util3 {
-	private static String authAlias="clientauth";
-	private static String cipherAlias="clientcipher";
-	private static String signAlias="clientsign";
+	private static String authAlias = "clientauth";
+	private static String cipherAlias = "clientcipher";
+	private static String signAlias = "clientsign";
 	private static String savePath = "/Users/lexy/Desktop/Clases/Seguridad/clientRecoveredFiles/";
+
 	public static void start(final Socket client2, String idRegistro, String pass_wd) {
 		// TODO Auto-generated method stub
 
@@ -41,7 +21,7 @@ public class Util3 {
 		new Thread() {
 			public void run() {
 				try {
-					String op = "3"; 
+					String op = "3";
 					DataOutputStream out;
 
 					out = new DataOutputStream(client2.getOutputStream());
@@ -50,11 +30,11 @@ public class Util3 {
 					out.flush();
 
 					Certificate certificate = Client.getKeyStore().getCertificate(authAlias);
-					byte [] certAuth= certificate.getEncoded();
+					byte[] certAuth = certificate.getEncoded();
 
-					X509Certificate extra= (X509Certificate) certificate ;
+					X509Certificate extra = (X509Certificate) certificate;
 					Principal idPropietario = extra.getIssuerDN();
-					System.out.println("ID PROPIETARIO: "+ idPropietario.toString());
+					System.out.println("ID PROPIETARIO: " + idPropietario.toString());
 
 					out.writeInt(certAuth.length);
 					out.write(certAuth);
@@ -63,47 +43,78 @@ public class Util3 {
 					out.write(idRegistro.getBytes());
 					out.flush();
 
-					ObjectInputStream input= new ObjectInputStream(client2.getInputStream());
+					ObjectInputStream input = new ObjectInputStream(client2.getInputStream());
 
 					Response response = (Response) input.readObject();
-					byte [] fileContent = null;
-					byte [] SignRDContent;
+					byte[] fileContent = null;
+					byte[] SignRDContent;
 
-					if(response.getError() == 0) {
-						if(Validation.validateCert(response.getCert(), Client.getTrust())) {
-							FileOutputStream file=new FileOutputStream(savePath+"file");
+					if (response.getError() == 0) {
+						if (Validation.validateCert(response.getCert(), Client.getTrust())) {
+							FileOutputStream file = new FileOutputStream(savePath + "file");
 
-							if(response.getIsPrivate()) {
+							if (response.getIsPrivate()) {
 								try {
-									file.write(
-											fileContent = Encription.decriptFilePGP(response.getEncriptedKey(), response.getEncriptedFile(), response.getCipherParams(), pass_wd, cipherAlias, Client.getKeyStore()));
+									file.write(fileContent = Encription.decriptFilePGP(response.getEncriptedKey(),
+											response.getEncriptedFile(), response.getCipherParams(), pass_wd,
+											cipherAlias, Client.getKeyStore()));
 
 								} catch (Exception e) {
 									System.out.println("Error desencriptando archivo: ");
 									e.printStackTrace();
 								}
-							}else {
+							} else {
 								fileContent = response.getNonEncriptedFile();
 							}
 
-							PrivateKey signkey = (PrivateKey) Client.getKeyStore().getKey(signAlias, pass_wd.toCharArray());
-							byte [] firmadoc = Validation.signContent(fileContent, signkey);
-							SignRDContent = getSignRDContent(response.getIdRegistro(),response.getSelloTemporal(), response.getIdPropietario(),fileContent, firmadoc);
-							if(Validation.checkSign(response.getCert(),SignRDContent, response.getSigRD())) {
+							PrivateKey signkey = (PrivateKey) Client.getKeyStore().getKey(signAlias,
+									pass_wd.toCharArray());
+
+							// Si se comprueba
+							byte[] firmadoc = Validation.signContent(fileContent, signkey);
+							SignRDContent = Validation.getSignRDContent(response.getIdRegistro(),
+									response.getSelloTemporal(), response.getIdPropietario(), fileContent, firmadoc);
+							/*
+							 * -> PROBLEMA CON EL ENUNCIADO!!
+							 * 
+							 * Entiendo que si un archivo es público, es porque queremos compartirlo con
+							 * otros cliente pero... Si SigRD =
+							 * SigR(idRegistro,selloTemporal,idPropietario,documento,firmaDoc) y firmaDoc =
+							 * Sigpropietario(documento), y como la respuesta de recuperacion de archivo:
+							 * (0,tipoConfidencialidad,idRegistro,idPropietario,selloTemporal,EPKC
+							 * (K),EK(documento),SigRD,CertFirm ), no incluye firmaDoc, significa que para
+							 * comprobar sigRD del lado del cliente para validarla, debes calcularla, pero
+							 * es necesario dirmaDoc, y para calcular esta es necesaria la clave privada del
+							 * cliente propietario que obviamente no tenemos, por lo tanto es imposible que
+							 * se pueda validar SigRD desde un cliente que no sea el propietario original.
+							 * 
+							 * Además un cliente no tiene por qué conocer el hash de un archivo subido por
+							 * otro cliente.
+							 * 
+							 * Para solucionarlo, aunque no es buena idea en un sistema real, se elimina la
+							 * necesidad de validación de sigRD si el archivo es publico. En el caso del
+							 * hash, en un sistema real tampoco se podría comprobar a menos que se enviase
+							 * en la respuesta, por lo tanto tambien se ignora si es publico.
+							 */
+
+							if (Validation.checkSign(response.getCert(), SignRDContent, response.getSigRD())
+									|| !response.getIsPrivate()) {
 								file.write(fileContent);
-								if(Client.checkHash(response.getIdRegistro(), fileContent)) {
+								if (Client.checkHash(response.getIdRegistro(), fileContent)
+										|| !response.getIsPrivate()) {
 									System.out.println("DOCUMENTO RECUPERADO CORRECTAMENTE");
-								}else{
+								} else {
 									System.out.println("DOCUMENTO ALTERADO POR EL REGISTRADOR");
 								}
-							}else {
+							} else {
 								System.out.println("FALLO DE FIRMA DEL REGISTRADOR");
 							}
+
 							file.close();
-						}else {
+						} else {
 							System.out.println("Certificado del servidor no valido");
 						}
-					}else{
+					} else {
 						System.out.println(response.getErrorMsg());
 					}
 
@@ -117,14 +128,4 @@ public class Util3 {
 		}.start();
 	}
 
-	public static byte[] getSignRDContent(int idRegistro, String selloTemporal,String idPropietario, byte[] nonEncriptedFile, byte[] firmaDoc) throws Exception {
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
-		outputStream.write(idRegistro);
-		outputStream.write(selloTemporal.getBytes());
-		outputStream.write(idPropietario.toString().getBytes());
-		outputStream.write(nonEncriptedFile);
-		outputStream.write(firmaDoc);
-
-		return outputStream.toByteArray();
-	}
 }
--- segB/src/server/Util3.java
+++ segB/src/server/Util3.java
@@ -8,79 +8,90 @@ import java.nio.file.*;
 import java.security.cert.*;
 import java.util.ArrayList;
 
-
 public class Util3 {
-	private static String savePath="/Users/lexy/Desktop/Clases/Seguridad/serverSavedFiles/";
-	private static String secretKeyAlias = "dataenckey";
-	private static String signAlias = "serversign";
+	private static final String SAVEPATH = Server.SAVEPATH;
+	private static final String SECRETKEYALIAS = Server.SECRETKEYALIAS;
+	private static final String SIGNALIAS = Server.SIGNALIAS;
 
-	public static void start(Socket aClient, String algorithm, String passwd_key) {
+	public static void retrieveFile(Socket aClient, String algorithm, String passwd_key) {
 		new Thread() {
 			public void run() {
 				try {
-					DataInputStream input= new DataInputStream(aClient.getInputStream());
+					DataInputStream input = new DataInputStream(aClient.getInputStream());
 
-					byte [] certAuth= input.readNBytes(input.readInt());
-					String id_registro=new String(input.readNBytes(input.readInt()));  
+					byte[] certAuth = input.readNBytes(input.readInt());
+					String id_registro = new String(input.readNBytes(input.readInt()));
 
 					InputStream in = new ByteArrayInputStream(certAuth);
-					CertificateFactory cf   = CertificateFactory.getInstance("X.509");
+					CertificateFactory cf = CertificateFactory.getInstance("X.509");
 					Certificate certificate = cf.generateCertificate(in);
-					Certificate serverCertSign = Server.getKeyStore().getCertificate(signAlias);
-
-					File directorio= new File (savePath);
-					String carpeta_comprobar = "";
-
+					Certificate serverCertSign = Server.getKeyStore().getCertificate(SIGNALIAS);
 
 					ObjectOutputStream response = new ObjectOutputStream(aClient.getOutputStream());
 					Response res = null;
 
-					boolean esprivado=false;
+					boolean esprivado = false;
 
-					if(Integer.parseInt(id_registro)<=Server.getContador()) {
-						File [] contenido =directorio.listFiles();
-						ArrayList <String>  contenido2 = new ArrayList <String>();
+					String idPropietario = ((X509Certificate) certificate).getIssuerDN().toString();
 
-						for(int i=0; i<contenido.length; i++) {
-							contenido2.add(contenido[i].getName());
-							if(contenido[i].getName().charAt(0)==(id_registro.charAt(0))) {
-								carpeta_comprobar=contenido[i].getName();
+					ArrayList<ArrayList<String>> fileList = DatabaseEntry.getFiles(SAVEPATH, idPropietario);
+					ArrayList<String> publico = fileList.get(0);
+					ArrayList<String> privado = fileList.get(1);
+
+					String fileToRetrieve = null;
+
+					// initContador esta preparado para soportar eliminacion de documentos a mano,
+					// pero no esta funcion!!
+					if (Integer.parseInt(id_registro) <= Server.getContador()) {
+						for (String fileName : privado) {
+							if (esprivado = fileName.startsWith(id_registro)) {
+								fileToRetrieve = fileName;
 							}
 						}
 
-						//Si se guardase en ficheros, comprobar por .cif
-						for(int i=0; i<Util.getPrivado().size(); i++) {
-							if(Util.getPrivado().get(i).charAt(0)==id_registro.charAt(0)) {
-								esprivado=true;
-							}
-						}
+						if (esprivado) {
+							if (Validation.validateCert(certificate, Server.getTrust())) {
+								if ((id_registro + "_" + idPropietario + ".sig.cif").equals(fileToRetrieve)) {
+									System.out.println("Propietario correcto, recuperando archivo: " + fileToRetrieve);
 
-						ArrayList<byte[]> req = new ArrayList <byte[]>();
+									ArrayList<byte[]> encripted2send = new ArrayList<byte[]>();
+									FileInputStream fileIn = new FileInputStream(
+											Paths.get(SAVEPATH, fileToRetrieve).toString());
+									ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+									DatabaseEntry dataEntry = (DatabaseEntry) objectIn.readObject();
 
-						String idPropietario = ((X509Certificate)certificate).getIssuerDN().toString();
-						if(esprivado) {
-							if(Validation.validateCert(certificate, Server.getTrust())) {
-								if ((id_registro+"_"+idPropietario).equals(carpeta_comprobar)) {
-									System.out.println("COMPROBACIÓN CORRECTA DE ID: " + carpeta_comprobar);
+									byte[] decriptedFile = Encription.decriptDocument(dataEntry.getContent(),
+											SECRETKEYALIAS, passwd_key, algorithm, dataEntry.getCipherParams(),
+											Server.getKeyStore());
 
-									req=es_privado(carpeta_comprobar);
-
-									byte [] decriptedFile = Encription.decriptDocument(req.get(5), secretKeyAlias, passwd_key, algorithm, Server.getLocalCipherParams(), Server.getKeyStore());
-									req = Encription.encript2sendPGP(req, decriptedFile, Server.getClientPublicKey());
-									res = new Response(Integer.parseInt(id_registro), idPropietario, req.get(3).toString(), req.get(6), req.get(7), req.get(8), req.get(4), serverCertSign);
-									
+									encripted2send = Encription.encript2sendPGP(decriptedFile,
+											dataEntry.getClientPublicKey());
+									res = new Response(Integer.parseInt(id_registro), idPropietario,
+											dataEntry.getSello(), encripted2send.get(0), encripted2send.get(1),
+											encripted2send.get(2), dataEntry.getSigRD(), serverCertSign);
 								} else {
 									res = new Response(-3);
 								}
-							}else {
+							} else {
+
 								res = new Response(-1);
 							}
+						} else {
+							for (String fileName : publico) {
+								if (fileName.startsWith(id_registro)) {
+									fileToRetrieve = fileName;
+								}
+							}
+
+							FileInputStream fileIn = new FileInputStream(
+									Paths.get(SAVEPATH, fileToRetrieve).toString());
+							ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+							DatabaseEntry dataEntry = (DatabaseEntry) objectIn.readObject();
+
+							res = new Response(Integer.parseInt(id_registro), idPropietario, dataEntry.getSello(),
+									dataEntry.getContent(), dataEntry.getSigRD(), serverCertSign);
 						}
-						else {
-							req=es_publico(carpeta_comprobar);
-							res = new Response(Integer.parseInt(id_registro), idPropietario, req.get(3).toString(), req.get(5),req.get(4), serverCertSign);
-						}
-					}else {
+					} else {
 						res = new Response(-4);
 					}
 					response.writeObject(res);
@@ -93,80 +104,5 @@ public class Util3 {
 				}
 			}
 		}.start();
-
-	}
-
-
-	public static ArrayList<byte[]> es_publico(String carpeta_comprobar) throws IOException{
-		ArrayList<byte[]> req = new ArrayList <byte[]>();
-		String ruta= savePath+carpeta_comprobar;
-
-		File directorio2= new File (ruta);
-		String [] names2= {"confidencialidad","firmaDocumento","idRegistro","selloTemporal","firmaSigRD"};
-		File [] contenido =directorio2.listFiles();
-
-		for(int i=0; i<names2.length; i++) {
-			for(int j=0; j<contenido.length; j++) {
-				if(names2[i].equals(contenido[j].getName())){
-					byte [] file =Files.readAllBytes(contenido[j].toPath());
-					req.add(file);
-				}
-			}
-		}
-
-		ArrayList <String>  names = new ArrayList <String>();	
-		for (int k=0; k<names2.length;k++) {
-			names.add(names2[k]);
-		}
-
-		ArrayList <File>  contenido2 = new ArrayList <File>();	
-
-		for(int i=0; i<contenido.length; i++) {
-			contenido2.add(contenido[i]);
-		}
-
-		for (int i=0; i< contenido2.size(); i ++) {
-			if (!names.contains(contenido2.get(i).getName())) {
-				byte [] file2 =Files.readAllBytes(contenido2.get(i).toPath());
-				req.add(file2);
-			}
-
-		}
-
-		return req;
-	}
-
-	public static ArrayList<byte[]> es_privado(String carpeta_comprobar) throws IOException{
-		ArrayList<byte[]> req = new ArrayList <byte[]>();
-		String ruta= savePath+carpeta_comprobar;
-		File directorio2= new File (ruta);
-		String [] names2= {"confidencialidad","firmaDocumento","idRegistro","selloTemporal","firmaSigRD"};
-		File [] contenido =directorio2.listFiles();
-
-		System.out.println("Archivos: "+ contenido.toString());
-
-		for(int i=0; i<names2.length; i++) {
-			byte [] file =Files.readAllBytes(Paths.get(ruta,names2[i]));
-			req.add(file);
-		}
-
-		ArrayList <String>  names = new ArrayList <String>();	
-		for (int k=0; k<names2.length;k++) {
-			names.add(names2[k]);
-		} 
-
-		ArrayList <File>  contenido2 = new ArrayList <File>();	
-
-		for(int i=0; i<contenido.length; i++) {
-			contenido2.add(contenido[i]);
-		}
-
-		for (int i=0; i< contenido2.size(); i ++) {
-			if (!names.contains(contenido2.get(i).getName())) {
-				byte [] file2 =Files.readAllBytes(contenido2.get(i).toPath());
-				req.add(file2);
-			}
-		}
-		return req;
 	}
 }
