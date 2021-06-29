1b848fb8fb01cb1a48a3e723192b406f31537ecb
janoglezcampos <janoglezcampos@gmail.com>
2021-06-29 22:43:04 +0200

    Los archivos se almacenan haciendo uso de DataBase entry en un unico archivo.

--- segB/src/server/Util.java
+++ segB/src/server/Util.java
@@ -4,130 +4,99 @@ import common.*;
 
 import java.io.*;
 import java.net.Socket;
-import java.security.KeyStore.*;
-import java.security.*;
+import java.nio.file.Paths;
 import java.security.cert.*;
 import java.security.cert.Certificate;
 import java.util.ArrayList;
-import java.util.List;
-
-import javax.crypto.*;
-import javax.crypto.spec.SecretKeySpec;
-
-import clientpart.Client;
-
-
 
 public class Util {
-	//byte [] recv;
-	//static byte [] array;
-	//static ArrayList <Byte>  datos = new ArrayList <Byte>();
-	private static ArrayList <String> privado = new ArrayList <String>();
-	private static ArrayList <String> publico = new ArrayList <String>();
-	private static String cipherAlias ="serverCipher";
-	private static String signAlias="serverSign";
-	private static String secretKeyAlias = "dataenckey";
-	private static String savePath="/Users/lexy/Desktop/Clases/Seguridad/serverSavedFiles/";
+	// byte [] recv;
+	// static byte [] array;
+	// static ArrayList <Byte> datos = new ArrayList <Byte>();
 
-	public static ArrayList<String> getPrivado() {
-		return privado;
-	}
+	private static final String CIPHERALIAS = Server.CIPHERALIAS;
+	private static final String SIGNALIAS = Server.SIGNALIAS;
+	private static final String SECRETKEYALIAS = Server.SECRETKEYALIAS;
+	private static final String SAVEPATH = Server.SAVEPATH;
 
-	public static void setPrivado(ArrayList<String> privado) {
-		Util.privado = privado;
-	}
-
-	public static ArrayList<String> getPublico() {
-		return publico;
-	}
-
-	public static void setPublico(ArrayList<String> publico) {
-		Util.publico = publico;
-	}
-	
 	public static void receiveFile(final Socket aClient, String algoritmo, String contrasenha) {
 		new Thread() {
 			public void run() {
 				try {
-					ArrayList <byte []> full = new ArrayList <byte[]>();
+					ArrayList<byte[]> full = new ArrayList<byte[]>();
 					full.clear();
 					char[] clave = contrasenha.toCharArray();
-					DataInputStream input= new DataInputStream(aClient.getInputStream());
+					DataInputStream input = new DataInputStream(aClient.getInputStream());
 					System.out.println("Input entrante");
-					String confidencialidad =new String (input.readNBytes(input.readInt()));
+					String confidencialidad = new String(input.readNBytes(input.readInt()));
 					Boolean isPrivate = confidencialidad.equals("PRIVADO");
-					System.out.println("Confidencialidad  "+confidencialidad);
-					int listSize=input.readInt();
-					
-					for(int i=0; i<listSize-2; i++) {
+					System.out.println("Confidencialidad  " + confidencialidad);
+					int listSize = input.readInt();
+
+					for (int i = 0; i < listSize - 2; i++) {
 						full.add(input.readNBytes(input.readInt()));
 					}
 					byte[] desencriptado;
 					if (isPrivate) {
-						desencriptado = Encription.decriptFilePGP(full.get(2), full.get(0), full.get(1), contrasenha, cipherAlias, Server.getKeyStore());
+						desencriptado = Encription.decriptFilePGP(full.get(2), full.get(0), full.get(1), contrasenha,
+								CIPHERALIAS, Server.getKeyStore());
 
 					} else {
-						desencriptado=full.get(0);
+						desencriptado = full.get(0);
 					}
 
 					byte[] sign = (isPrivate) ? full.get(3) : full.get(1);
 					byte[] cerFirma = (isPrivate) ? full.get(4) : full.get(2);
 
-
-					CertificateFactory cf   = CertificateFactory.getInstance("X.509");
+					CertificateFactory cf = CertificateFactory.getInstance("X.509");
 					InputStream cipherStream = new ByteArrayInputStream(input.readNBytes(input.readInt()));
 					Certificate cipherCert = cf.generateCertificate(cipherStream);
-					Server.setClientPublicKey(cipherCert.getPublicKey());
-					
+
 					String originalFileName = new String(input.readNBytes(input.readInt()));
-					
+
 					System.out.println("Comprobando firma");
 
 					Certificate certificate = cf.generateCertificate(new ByteArrayInputStream(cerFirma));
 					ObjectOutputStream responseStream = new ObjectOutputStream(aClient.getOutputStream());
-					
-					if(Validation.validateCert(certificate, Server.getTrust())) {
-						if (Validation.checkSign(certificate, desencriptado,sign)) {
-							//PASAMOS AL GUARDADO DEL ARCHIVO
+
+					if (Validation.validateCert(certificate, Server.getTrust())) {
+						if (Validation.checkSign(certificate, desencriptado, sign)) {
+							// PASAMOS AL GUARDADO DEL ARCHIVO
 							System.out.println("\nFirma verificada.");
 							System.out.println("Procedemos a guardar el archivo");
-							FileSave file = new FileSave(cerFirma, desencriptado, sign,clave,isPrivate,savePath,signAlias);
-							String fileName = file.getFileName();
+							DatabaseEntry dataEntry = new DatabaseEntry(Server.getContador(), isPrivate,
+									originalFileName, certificate, desencriptado, sign, Server.getKeyStore(), SIGNALIAS,
+									clave, cipherCert.getPublicKey());
 
-							String ruta_save=savePath+fileName+"/"+originalFileName;
-							FileOutputStream filedef =new FileOutputStream(ruta_save);
-							// Comprobamos confidencialidad 
+							FileOutputStream fileOut = new FileOutputStream(
+									Paths.get(SAVEPATH, dataEntry.getFileName()).toString());
+							ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
 
 							if (isPrivate) {
-								
-								ArrayList<byte[]> tuple = Encription.encriptDocument(desencriptado, secretKeyAlias, contrasenha, algoritmo, Server.getKeyStore());
-								
-								Server.setLocalCipherParams(tuple.get(0));
-								
-								privado.add(fileName);
-								filedef.write(tuple.get(1));
-								filedef.close();
 
-								responseStream.writeObject(file.getResponse());
-								System.out.println("Server: Archivo guardado"); 
-							}
-							else
-							{
-								publico.add(fileName);
-								filedef.write(desencriptado);
-								filedef.close();
+								ArrayList<byte[]> tuple = Encription.encriptDocument(desencriptado, SECRETKEYALIAS,
+										contrasenha, algoritmo, Server.getKeyStore());
+								dataEntry.addFileContent(tuple.get(1), tuple.get(0));
+								objectOut.writeObject(dataEntry);
+								responseStream.writeObject(dataEntry.getResponse(Server.getKeyStore(), SIGNALIAS));
 
-								responseStream.writeObject(file.getResponse());
+								// responseStream.writeObject(file.getResponse());
+								System.out.println("Server: Archivo guardado");
+							} else {
+								dataEntry.addFileContent(desencriptado);
+								objectOut.writeObject(dataEntry);
+								responseStream.writeObject(dataEntry.getResponse(Server.getKeyStore(), SIGNALIAS));
 
 								System.out.println("Server: Archivo guardado");
 							}
+							Server.incremetarContador();
 						} else {
-							//Firma incorrecta
+							// Firma incorrecta
 							System.out.println("\nFirma incorrecta.");
 							responseStream.writeObject(new Response(-2));
 						}
-					}else {
-						//Certificado invalido
+					} else {
+						// Certificado invalido
 						responseStream.writeObject(new Response(-1));
 					}
 					responseStream.close();
